4988bac102fcd413f0abea69f7d95762e059a685
luozhiwei <zhiwei.luo@360che.com>
2022-06-23 15:13:10 +0800

    增加接口文档&sql文档

--- models/mysql.go
+++ models/mysql.go
@@ -5,12 +5,12 @@ import (
 )
 import _ "github.com/go-sql-driver/mysql"
 
-func init()  {
+func init() {
 	// 开始sql调试
 	orm.Debug = true
 	// 注册models
 	orm.RegisterModel(new(Category))
 	orm.RegisterModel(new(User))
 	// 注册数据库
-	_ = orm.RegisterDataBase("default", "mysql", "root:p9njkbt9@tcp(101.200.215.43)/open?charset=utf8")
+	_ = orm.RegisterDataBase("default", "mysql", "")
 }
